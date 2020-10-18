469d4264d92fd6bf24087684e4427be9d075114a
Samuel Lord <sllord@icloud.com>
2020-10-18 17:23:45 -0500

    Update v2-alpha.lua

--- v2-alpha.lua
+++ v2-alpha.lua
@@ -1,8 +1,21 @@
-local admins = {"SparksammyOfficial", "BlocksWatcher11"}
+-- Sparksammy Admin Commands v2 Alpha
+-- Licensed under MIT license. :-)
+local admins = {"SparksammyOfficial", "BlocksWatcher11", "louponius"}
+local bansList = {"Vuqle", "someoneyoudislike"} --haha lol get noob
 local children = game.Players:GetChildren()
 local admin = nil --Default admin is nothing
 local player = nil --Default Player is nothing
 print("Admin script begin")
+
+if table.find(bansList, script.Parent.Name) then
+	script.Parent:Kick("List-banned!")
+end
+
+local function ban(plyr)
+	while wait(1) do
+		plyr:Kick("Ban hammer has spoken!")
+	end
+end
 
 if table.find(admins, script.Parent.Name) ~= nil then
 	print("Admin found! Name: " .. script.Parent.Name)
@@ -21,6 +34,12 @@ if table.find(admins, script.Parent.Name) ~= nil then
 			elseif string.match(message, "$kill") then
 				local char = game.Players[string.gsub(message, "$kill ", "")].Character
 				char.Humanoid.Health = 0
+			elseif string.match(message, "$kick") then
+				local plr = game.Players[string.gsub(message, "$kick ", "")]
+				plr:Kick()
+			elseif string.match(message, "$ban") then
+				local plr = game.Players[string.gsub(message, "$ban ", "")]
+				coroutine.resume(coroutine.create(ban(plr)))
 			else
 				print("Command not found!")
 			end
